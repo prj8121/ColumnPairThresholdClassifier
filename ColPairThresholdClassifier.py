9e5c0a36e015c425bb11f3a8cdd23e764facb846
Parker <nationalparx@gmail.com>
2023-05-08 14:04:06 -0700

    Branch to rip out old stuff to ease transition to sklearn standards

--- ColPairThresholdClassifier.py
+++ ColPairThresholdClassifier.py
@@ -1,23 +1,49 @@
 # Parker Johnson
 
 import pandas as pd
+import numpy as np
 import time
 import matplotlib.pyplot as plt
 from bestLineFinder import get_best_lines
+from sklearn.base import BaseEstimator, ClassifierMixin
 
-class ColPairThresholdClassifier:
+class ColPairThresholdClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self):
         self.model = {}
+        self.class_map = {}
+        self.col_map = {}
         
     def fit(self, data_cols, target_col, timer=False):
-        setattr(self, 'data_col_names', data_cols.columns)      # Names of all columns besides target
-        setattr(self, 'target_col_name', target_col.columns[0]) # Name of the target column
-        setattr(self, 'species_vals', target_col.unique())      # Unique values represented in the target column
+        match type(data_cols):
+            case pd.DataFrame:
+                setattr(self, 'data_col_names', data_cols.columns)      # Names of all columns besides target
+                setattr(self, 'target_col_name', target_col.name)       # Name of the target column
+                setattr(self, 'class_names', target_col.unique())       # Unique values represented in the target column
+            case np.ndarray:
+                setattr(self, 'data_col_names', [f'col{x}' for x in range(len(data_cols[0]))])      # Names of all columns besides target
+                setattr(self, 'target_col_name', 'target')       # Name of the target column
+                class_names = []
+                for val in np.unique(target_col):
+                    self.class_map[f'col{val}'] = val           # This is an artifact of how I initially implemented clashing with sklearn standards
+                    class_names.append(val)
+                setattr(self, 'class_names', class_names)      # Unique values represented in the target column
+                
+            case _:
+                raise(TypeError('First argument must be of type pd.DataFrame or np.ndarray'))
+
+        target_col = 
+        print(self.class_names)
+        print(target_col)
+        #setattr(self, 'data_col_names', data_cols.columns)      # Names of all columns besides target
+        #setattr(self, 'target_col_name', target_col.name)       # Name of the target column
+        #setattr(self, 'species_vals', target_col.unique())      # Unique values represented in the target column
+
+
 
         for col_1 in range(len(self.data_col_names)):
             for col_2 in range(col_1, len(self.data_col_names)):
                 start_time = time.time()
-                lines = get_best_lines(data_cols[self.data_col_names[col_1]], data_cols[self.data_col_names[col_2]], target_col, self.species_vals)
+                lines = get_best_lines(data_cols[self.data_col_names[col_1]], data_cols[self.data_col_names[col_2]], target_col, self.class_names)
                 self.model[(self.data_col_names[col_1], self.data_col_names[col_2])] = lines
                 end_time = time.time()
                 if timer:
@@ -29,10 +55,12 @@ class ColPairThresholdClassifier:
     def predict(self, data_cols):
         return [self.predict_on_row(row) for index, row in data_cols.iterrows()]
 
+    
+
     def load_data(self, df, target, timer=False):
         setattr(self, 'data_cols', df.columns.drop(target)) # Names of all columns besides target
         setattr(self, 'target_col', target)                 # Name of the target column
-        setattr(self, 'species_vals', df[target].unique())  # Unique values represented in the target column
+        setattr(self, 'class_names', df[target].unique())  # Unique values represented in the target column
 
         if not target in df.columns: # TODO make this a real error
             print("Target does not exist in the dataframe")
@@ -41,7 +69,7 @@ class ColPairThresholdClassifier:
         for col_1 in range(len(self.data_cols)):
             for col_2 in range(col_1, len(self.data_cols)):
                 start_time = time.time()
-                lines = get_best_lines(df[self.data_cols[col_1]], df[self.data_cols[col_2]], df[target], self.species_vals)
+                lines = get_best_lines(df[self.data_cols[col_1]], df[self.data_cols[col_2]], df[target], self.class_names)
                 self.model[(self.data_cols[col_1], self.data_cols[col_2])] = lines
                 end_time = time.time()
                 if timer:
@@ -59,7 +87,7 @@ class ColPairThresholdClassifier:
         data_cols = self.data_cols
 
         # TODO deal with color mapping 
-        species_vals = self.species_vals
+        class_names = self.class_names
         color_map = {'Iris-setosa':(1,0,0), 'Iris-versicolor':(0,1,0), 'Iris-virginica':(0,0,1)}
         colors = raw_data['species'].map(color_map)
 
@@ -109,8 +137,8 @@ class ColPairThresholdClassifier:
     
 
     def predict_on_row(self, row: pd.Series):
-        if self.target_col in row:
-            row = row.drop(self.target_col)
+        # if self.target_col_name in row:
+        #    row = row.drop(self.target_col_name)
 
         row_keys = row.keys()
         
