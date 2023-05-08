1d6ef89c8f13761fa40529a7a853a4f01f513e59
Parker <nationalparx@gmail.com>
2023-05-08 14:07:52 -0700

    removed artifacts of the old ways

--- ColPairThresholdClassifier.py
+++ ColPairThresholdClassifier.py
@@ -10,8 +10,6 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 class ColPairThresholdClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self):
         self.model = {}
-        self.class_map = {}
-        self.col_map = {}
         
     def fit(self, data_cols, target_col, timer=False):
         match type(data_cols):
@@ -55,86 +53,6 @@ class ColPairThresholdClassifier(BaseEstimator, ClassifierMixin):
     def predict(self, data_cols):
         return [self.predict_on_row(row) for index, row in data_cols.iterrows()]
 
-    
-
-    def load_data(self, df, target, timer=False):
-        setattr(self, 'data_cols', df.columns.drop(target)) # Names of all columns besides target
-        setattr(self, 'target_col', target)                 # Name of the target column
-        setattr(self, 'class_names', df[target].unique())  # Unique values represented in the target column
-
-        if not target in df.columns: # TODO make this a real error
-            print("Target does not exist in the dataframe")
-            return
-
-        for col_1 in range(len(self.data_cols)):
-            for col_2 in range(col_1, len(self.data_cols)):
-                start_time = time.time()
-                lines = get_best_lines(df[self.data_cols[col_1]], df[self.data_cols[col_2]], df[target], self.class_names)
-                self.model[(self.data_cols[col_1], self.data_cols[col_2])] = lines
-                end_time = time.time()
-                if timer:
-                    print(f'Line fitting took {end_time - start_time} seconds for column pair {self.data_cols[col_1]}, {self.data_cols[col_2]}')
-
-        if timer:
-            print("Model loaded")
-
-
-    def plot_data(self, df, raw_data):
-        if not self.model:
-            print("No model Error") #TODO actually throw an error 
-            return
-
-        data_cols = self.data_cols
-
-        # TODO deal with color mapping 
-        class_names = self.class_names
-        color_map = {'Iris-setosa':(1,0,0), 'Iris-versicolor':(0,1,0), 'Iris-virginica':(0,0,1)}
-        colors = raw_data['species'].map(color_map)
-
-        plt.figure(figsize=(12, 12))
-
-        for col_1 in range(len(data_cols)):
-            for col_2 in range(col_1, len(data_cols)):
-                lines = self.model[(data_cols[col_1], data_cols[col_2])]
-
-                points = {}
-                x_min = df[data_cols[col_1]].min()
-                x_max = df[data_cols[col_1]].max()
-                y_min = df[data_cols[col_2]].min()
-                y_max = df[data_cols[col_2]].max()
-
-                for c in lines.keys():
-
-                    p_left = (x_min, (lines[c].slope * x_min) + lines[c].intercept)
-                    p_right = (x_max, (lines[c].slope * x_max) + lines[c].intercept)
-                    p_top = ((y_max - lines[c].intercept) / lines[c].slope , y_max)
-                    p_bottom = ((y_min - lines[c].intercept) / lines[c].slope , y_min)
-
-                    p_list = set([p_left, p_right, p_top, p_bottom])
-
-                    res = []
-                    for p in p_list:
-                        if x_min <= p[0] and p[0] <= x_max:
-                            if y_min <= p[1] and p[1] <= y_max:
-                                res.append(p)
-
-                    points[c] = res
-
-                plt.subplot(4,4, (col_1 * 4) + col_2 + 1)
-                plt.scatter(df[data_cols[col_1]], df[data_cols[col_2]], s=7, c=colors)
-                for c in lines.keys():
-                    plt.plot([points[c][0][0], points[c][1][0]], [points[c][0][1], points[c][1][1]], c=color_map[c], marker= '^' if lines[c].direction else 'v')
-                plt.title(f'{data_cols[col_1]}, {data_cols[col_2]}')
-
-
-    def test(self, test_data: pd.DataFrame):
-        if not self.model:
-            print("No model Error") #TODO actually throw an error 
-            return []
-        for index, row in test_data.iterrows():
-            prediction = self.predict_on_row(row)
-            print(prediction)
-    
 
     def predict_on_row(self, row: pd.Series):
         # if self.target_col_name in row:
