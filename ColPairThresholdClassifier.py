d23a7e45e71f46fb790efa00430a76ab50621b32
Parker <nationalparx@gmail.com>
2023-05-08 15:40:01 -0700

    removed unused code

--- ColPairThresholdClassifier.py
+++ ColPairThresholdClassifier.py
@@ -21,15 +21,14 @@ class ColPairThresholdClassifier(BaseEstimator, ClassifierMixin):
 
         for col_1 in range(num_cols):
             for col_2 in range(col_1, num_cols):
-                start_time = time.time()
+                if timer:
+                    start_time = time.time()
 
-                #print([data_cols[:, col_1], data_cols[:, col_2], target_col, np.unique(target_col)])
-                # [f'class{x}' for x in np.unique(target_col)]
                 lines = get_best_lines(data_cols[:, col_1], data_cols[:, col_2], target_col, np.unique(target_col))
                 self.model[(col_1, col_2)] = lines
-                print("Got through")
-                end_time = time.time()
+                
                 if timer:
+                    end_time = time.time()
                     print(f'Line fitting took {end_time - start_time} seconds for column pair {col_1}, {col_2}')
 
         if timer:
@@ -66,34 +65,4 @@ class ColPairThresholdClassifier(BaseEstimator, ClassifierMixin):
             result.append(c_with_highest_score)
 
         return result
-        
-
-
-    def predict_on_row(self, row: pd.Series):
-        # if self.target_col_name in row:
-        #    row = row.drop(self.target_col_name)
-
-        row_keys = row.keys()
-        
-        for col_1_i in range(len(row_keys)):
-            for col_2_i in range(col_1_i, len(row_keys)):
-                col_1 = row_keys[col_1_i]
-                col_2 = row_keys[col_2_i]
-                lines = self.model[(col_1, col_2)]
-                
-                row_x = row[col_1]
-                row_y = row[col_2]
-                
-                prediction = {}
-                
-                for c in lines.keys():
-                    
-                    above = 1 if row_y > (lines[c]['slope'] * row_x) + lines[c]['intercept'] else -1
-                    direction = 1 if lines[c]['direction'] else -1
-                    if prediction.get(c):
-                        prediction[c] = prediction[c] + (lines[c]['score'] * direction * above)
-                    else:
-                        prediction[c] = lines[c]['score'] * direction * above
-        
-        c_with_highest_score = max(prediction, key=prediction.get)
-        return c_with_highest_score
+    
